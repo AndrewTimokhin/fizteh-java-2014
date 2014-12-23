1961c1a1d5167091279115ff84af39ed7aec3743
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2014-12-23 23:46:31 +0300

    Update TableDriver.java

--- src/ru/fizteh/fivt/students/AndrewTimokhin/MultiFileHashMap v2.0/TableDriver.java
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/MultiFileHashMap v2.0/TableDriver.java
@@ -4,34 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TableDriver {
-	private String name;
-	public Map<String, Object>[] map;
+   private String name;
+   public Map<String, Object>[] map;
 
-	TableDriver() {
+   TableDriver() {
 
-	}
+   }
 
-	TableDriver(String name) {
-		this.name = name;
-		map = (Map<String, Object>[]) new Map[16];
-		for (int i = 0; i < 16; i++) {
-			map[i] = new HashMap<String, Object>();
-		}
+   TableDriver(String name) {
+      this.name = name;
+      map = (Map<String, Object>[]) new Map[16];
+      for (int i = 0; i < 16; i++) {
+         map[i] = new HashMap<String, Object>();
+      }
 
-	}
+   }
 
-	public int size() {
-		int summ = 0;
-		if (map != null) {
-			for (int i = 0; i < 16; i++) {
-				if (map[i] != null)
-					summ += map[i].size();
-			}
-		}
-		return summ;
-	}
+   public int size() {
+      int summ = 0;
+      if (map != null) {
+         for (int i = 0; i < 16; i++) {
+            if (map[i] != null) {
+               summ += map[i].size();
+            }
+         }
+      }
+      return summ;
+   }
 
-	public String getName() {
-		return name;
-	}
+   public String getName() {
+      return name;
+   }
 }
