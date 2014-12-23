2b10f9042e1bd468e0572a45179859c2514e4a37
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2014-12-23 23:46:12 +0300

    Update TableWork.java

--- src/ru/fizteh/fivt/students/AndrewTimokhin/MultiFileHashMap v2.0/TableWork.java
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/MultiFileHashMap v2.0/TableWork.java
@@ -5,157 +5,159 @@ import java.security.AccessControlException;
 import java.util.*;
 
 public class TableWork {
-	public TableDriver t[];
-	String homeDir;
-	int indicator;
-	int size;
+   public TableDriver [] t ;
+   String homeDir;
+   int indicator;
+   int size;
 
-	TableWork(int size, String homeDir) {
-		Reader rd = new Reader();
-		this.size = 0;
-		File input = new File(homeDir);
-		String[] st;
-		st = input.list();
-		if (st != null) {
-			for (String path : st) {
+   TableWork(int size, String homeDir) {
+      Reader rd = new Reader();
+      this.size = 0;
+      File input = new File(homeDir);
+      String[] st;
+      st = input.list();
+      if (st != null) {
+         for (String path : st) {
 
-				File tm2 = new File(homeDir + "\\" + path);
+            File tm2 = new File(homeDir + "\\" + path);
 
-				if (tm2.list() != null) {
+            if (tm2.list() != null) {
 
-					if (size == 0) {
-						t = new TableDriver[1];
-						t[0] = new TableDriver(path);
-						t[0].map = rd.read(homeDir + "\\" + path);
+               if (size == 0) {
+                  t = new TableDriver[1];
+                  t[0] = new TableDriver(path);
+                  t[0].map = rd.read(homeDir + "\\" + path);
 
-					} else {
-						TableDriver temp[] = new TableDriver[t.length + 1];
-						for (int k = 0; k < t.length; k++)
-							temp[k] = t[k];
+               } else {
+                  TableDriver [] temp = new TableDriver[t.length + 1];
+                  for (int k = 0; k < t.length; k++) {
+                      temp[k] = t[k]; 
+                     }
+                  temp[t.length] = new TableDriver(path);
+                  temp[t.length].map = rd.read(homeDir + "\\" + path);
 
-						temp[t.length] = new TableDriver(path);
-						temp[t.length].map = rd.read(homeDir + "\\" + path);
+                  t = temp;
+               }
+               size++;
+            }
 
-						t = temp;
-					}
-					size++;
-				}
+         }
+      }
 
-			}
-		}
+      this.homeDir = homeDir;
 
-		this.homeDir = homeDir;
+      indicator = -1;
+   }
 
-		indicator = -1;
-	}
+   public int setDir = -1; // by default
 
-	public int setDir = -1; // by default
+   public int getset() {
+      return setDir;
+   }
 
-	public int getset() {
-		return setDir;
-	}
+   public int create(String tableToAdd) throws NullPointerException {
 
-	public int create(String tableToAdd) throws NullPointerException {
+      if (t != null) {
+         for (int i = 0; i < t.length; i++) {
+            if (tableToAdd.equals(t[i].getName())) {
+               System.out.println("exist!");
+               return 1;
+            }
+         }
+                  {
+            TableDriver [] temp  = new TableDriver[t.length + 1];
+            for (int k = 0; k < t.length; k++) {
+                 temp[k] = t[k]; 
+               }
 
-		if (t != null) {
-			for (int i = 0; i < t.length; i++) {
-				if (tableToAdd.equals(t[i].getName())) {
-					System.out.println("exist!");
-					return 1;
-				}
-			}
-			;
-			{
+            temp[t.length] = new TableDriver(tableToAdd);
+            t = temp;
+            // System.out.println("created");
+         }
+      }
 
-				TableDriver temp[] = new TableDriver[t.length + 1];
-				for (int k = 0; k < t.length; k++)
-					temp[k] = t[k];
+      if (t == null || size == 0) {
+         t = new TableDriver[1];
+         t[0] = new TableDriver(tableToAdd);
+      }
+      System.out.println("created");
+      size++;
+      return 0; // if added
+   }
 
-				temp[t.length] = new TableDriver(tableToAdd);
-				t = temp;
-				// System.out.println("created");
-			}
-		}
+   public void remove(File anyfile) {
+      if (!anyfile.exists()) {
+         return; 
+         }
+      if (anyfile.isDirectory()) {
+         for (File f : anyfile.listFiles()) {
+            remove(f); }
+         anyfile.delete();
+      } else {
+         anyfile.delete();
+      }
+   }
 
-		if (t == null || size == 0) {
-			t = new TableDriver[1];
-			t[0] = new TableDriver(tableToAdd);
-		}
-		System.out.println("created");
-		size++;
-		return 0; // if added
-	}
+   public void drop(String tableToDel) throws NullPointerException,
+         IOException {
+      int haveDir = 0;
+      for (int i = 0; i < t.length; i++) {
+         if (t[i] != null && tableToDel.equals(t[i].getName())) {
 
-	public void remove(File anyfile) {
-		if (!anyfile.exists())
-			return;
-		if (anyfile.isDirectory()) {
-			for (File f : anyfile.listFiles())
-				remove(f);
-			anyfile.delete();
-		} else {
-			anyfile.delete();
-		}
-	}
+            if (t.length != 1) {
+               TableDriver tm = new TableDriver();
+               t[i] = t[t.length - 1];
+               t[t.length - 1] = null;
 
-	public void drop(String tableToDel) throws NullPointerException,
-			IOException {
-		int haveDir = 0;
-		for (int i = 0; i < t.length; i++) {
-			if (t[i] != null && tableToDel.equals(t[i].getName()) == true) {
+            } else {
+               t[i] = null; 
+               }
+            size--;
+            System.out.println(tableToDel + " dropped");
+            File in = new File(homeDir);
+            String [] st = in.list();
 
-				if (t.length != 1) {
-					TableDriver tm = new TableDriver();
-					t[i] = t[t.length - 1];
-					t[t.length - 1] = null;
+            if (st != null) {
+               for (String time : st) {
+                  if (time.equals(tableToDel)) {
+                     try {
+                        in = new File(homeDir + "\\" + tableToDel);
 
-				} else
-					t[i] = null;
-				size--;
-				System.out.println(tableToDel + " dropped");
-				File in = new File(homeDir);
-				String st[] = in.list();
+                        remove(in);
+                     } catch (AccessControlException e) {
+                        System.out.println("Access forbidden!");
+                     }
 
-				if (st != null) {
-					for (String time : st) {
-						if (time.equals(tableToDel)) {
-							try {
-								in = new File(homeDir + "\\" + tableToDel);
+                  }
 
-								remove(in);
-							} catch (AccessControlException e) {
-								System.out.println("Access forbidden!");
-							}
+               }
+            }
 
-						}
+            // if was deleted
+            break;
+         }
 
-					}
-				}
+      }
+      if (haveDir == 0) {
+         System.out.println("not exists");
 
-				// if was deleted
-				break;
-			}
+      }
 
-		}
-		if (haveDir == 0) {
-			System.out.println("not exists");
+   }
 
-		}
+   public int userUse(String tablename) throws NullPointerException {
+      if (t != null && t[0] != null) {
+         for (int i = 0; i < t.length; i++) {
+            if (t[i].getName().equals(tablename)) {
+               indicator = i;
+               System.out.println("use");
+               return 0;
+            }
+         } 
+         }
+      System.out.println("not exist");
+      return 1;
 
-	}
-
-	public int userUse(String tablename) throws NullPointerException {
-		if (t != null && t[0] != null)
-			for (int i = 0; i < t.length; i++) {
-				if (t[i].getName().equals(tablename)) {
-					indicator = i;
-					System.out.println("use");
-					return 0;
-				}
-			}
-		System.out.println("not exist");
-		return 1;
-
-	}
+   }
 
 }
