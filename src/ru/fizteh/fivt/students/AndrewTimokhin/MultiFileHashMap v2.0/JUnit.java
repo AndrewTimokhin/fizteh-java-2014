174cbadb63dc4c5b83bd4663345451d2356a7564
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2014-12-23 23:47:24 +0300

    Update JUnit.java

--- src/ru/fizteh/fivt/students/AndrewTimokhin/MultiFileHashMap v2.0/JUnit.java
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/MultiFileHashMap v2.0/JUnit.java
@@ -4,22 +4,22 @@ import java.io.*;
 import java.util.*;
 
 public class JUnit {
-	public static void main(String[] args) throws NullPointerException,
-			IOException, IllegalAccessException {
-		String path = System.getProperty("fizteh.db.dir").toString();
-		TableWork tb = new TableWork(0, path);
-		Functional fn = new Functional();
+   public static void main(String[] args) throws NullPointerException,
+         IOException, IllegalAccessException {
+      String path = System.getProperty("fizteh.db.dir").toString();
+      TableWork tb = new TableWork(0, path);
+      Functional fn = new Functional();
 
-		ModeWork mw = new ModeWork();
+      ModeWork mw = new ModeWork();
 
-		mw.usermode(fn, tb);
-		Write wr = new Write();
-		if (tb.t != null)
-			for (int i = 0; i < tb.t.length; i++) {
+      mw.usermode(fn, tb);
+      Write wr = new Write();
+      if (tb.t != null) {
+         for (int i = 0; i < tb.t.length; i++) {
 
-				wr.write(tb.t[i], tb);
-			}
-
-	};
+            wr.write(tb.t[i], tb);
+         }
+      }
+   };
 
 }
