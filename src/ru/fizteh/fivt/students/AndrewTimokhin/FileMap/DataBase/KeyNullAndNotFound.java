44da6c6251b5aea6f398d1e122a28d7d606bb29d
AndrewTimokhin <AndrewTimokhin@users.noreply.github.com>
2015-02-03 05:11:17 +0300

    Update KeyNullAndNotFound.java

--- src/ru/fizteh/fivt/students/AndrewTimokhin/FileMap/DataBase/KeyNullAndNotFound.java
+++ src/ru/fizteh/fivt/students/AndrewTimokhin/FileMap/DataBase/KeyNullAndNotFound.java
@@ -1,6 +1,5 @@
 package ru.fizteh.fivt.students.AndrewTimokhin.FileMap.DataBase;
 
-@SuppressWarnings("serial")
 public class KeyNullAndNotFound extends Exception {
 
     public KeyNullAndNotFound(String description) {
